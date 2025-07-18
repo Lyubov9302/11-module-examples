0f7bddd12a7e4c3478689af0754c0d28caf77441
Lyubov9302 <lyubovyurynets@gmail.com>
2025-07-18 19:32:32 +0300

    +++

---
+++ src/01-axios.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import '../common.css';
+
+// Додаємо та використовуємо бібліотеку https://axios-http.com/
+// Робимо рефакторинг
+// - Імпорт
+// - Запит
+// - Метод axios.get
+// - Обробка відповіді та помилки
+
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+const list = document.querySelector(".todo-list");
+
+// fetch(url)
+// .then(res => {
+//     if(!res.ok) {
+//         throw new Error("Oops");
+//     }
+//     return res.json();
+// })
+// .then(data => {
+//     console.log(data);
+//     list.insertAdjacentHTML("beforeend", createMarkup(data))
+// })
+// .catch(error => {
+//     console.log(error);
+// })
+
+
+
+
+function createMarkup(arr) {
+    return arr.map(({title, completed}) => `
+    <li class="list-item">
+    <input type="checkbox" ${completed && "checked"}/>
+    <p>${title}</p>
+    </li>
+    `).join("");
+}
+
+// axios("https://jsonplaceholder.typicode.com/todos")
+// .then(res => {
+//     console.log(res);
+//     list.insertAdjacentHTML("beforeend", createMarkup(data));
+// })
+// .catch(error => {
+//     console.log(error.message);
+// })
+
+axios.get("https://jsonplaceholder.typicode.com/todos")
+.then(res => {
+        console.log(res);
+        list.insertAdjacentHTML("beforeend", createMarkup(data));
+    })
+    .catch(error => {
+        console.log(error.message);
